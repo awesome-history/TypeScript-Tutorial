dd71961946e1f1a5d6692bcaa033e63f766b2578
K-connor <k.connor614@gmail.com>
2020-08-19 21:25:40 +0900

    기본 타입형태 정리 및 noImplicitAny 옵션 설정

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-class Test{
+class Test {
     constructor() {
         console.log('test');
     }
@@ -9,3 +9,32 @@ new Test();
 let testName = '123';
 testName.toString()
 
+
+// boolean은 프리미티어 타입인 소문자로 사용해야한다.
+let bool: boolean = false;
+
+// number는 프리미티어 타입인 소문자로 사용해야한다.
+let num: number = 11;
+
+// string을 사용할때 `를 권장한다.
+let str: string = `fadsfas`;
+
+// 사용하면 안된다. any를 사용하지 않는것이 TS를 올바르게 사용하는것과 같다.
+// let any : any = false;
+
+
+// interface 선언
+interface Person {
+    name: string;
+    age: number;
+}
+
+// p에 대입하여 사용
+const p: Person = {
+    name: `Min`,
+    age: 26,
+}
+
+// Array를 통한 선언의 방식 2가지
+// Person[];
+// Array<Person>
