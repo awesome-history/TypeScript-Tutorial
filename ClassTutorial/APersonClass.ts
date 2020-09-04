e0bf256b45c0431d62740f107f2237fcceab41ea
K-connor <k.connor614@gmail.com>
2020-09-04 22:37:16 +0900

    SingletonClass 추가 처리.

--- ClassTutorial/APersonClass.ts
+++ ClassTutorial/APersonClass.ts
@@ -1,13 +1,13 @@
-
 // abstract class를 구현이 가능하다.
 // !: << undefined 도 가능하다는 구문이다.
 abstract class APersonClass {
     protected name !: string;
 
-    abstract setName(name :string): void;
+    abstract setName(name: string): void;
 }
 
 // APersonTestClass 확장을 받아서 사용하면 구현이 된다.
+// tslint:disable-next-line:max-classes-per-file
 class APersonTestClass extends APersonClass {
     setName(name: string) {
         this.name = name;
---
+++ ClassTutorial/SingletonClass.ts
@@ -0,0 +1,21 @@
+
+// private 생성자를 이용해서 내부에서만 인스턴스가 생성이 가능하도록 한다.
+// 공개된 getInstance()를 통하여 static instance를 얻는다.
+// Lazy Loading가 되어 최초 실행시가 아닌, 할당을 할때 사용한다.
+class SingletonTestClass {
+    public static getInstance(): SingletonTestClass | null {
+        if (SingletonTestClass.instance === null) {
+            SingletonTestClass.instance = new SingletonClass();
+        }
+
+        return SingletonTestClass.instance;
+    }
+
+    private static instance: SingletonTestClass | null = null;
+
+    private constructor() {
+    }
+}
+
+const PSingleton: SingletonClass = SingletonTestClass.getInstance();
+
