643d0c42dc1815b649e43a1f3128264c6c650739
K-connor <k.connor614@gmail.com>
2020-09-07 23:29:17 +0900

    Generic 관련 학습 내용 추가.

--- GenericTutorial/GenericLookupClass.ts
+++ GenericTutorial/GenericLookupClass.ts
@@ -8,6 +8,8 @@ const gli: GenericLookupInterface = {
     age: 10,
 }
 
+// extends keyof 의 경우..
+// GenericLookupInterface 기준 "name", "age" 노출이 된다.
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
 }
@@ -15,3 +17,7 @@ function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
 function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]): void {
     obj[key] = value;
 }
+
+// age 또는 name만 적용할 수 있다.
+// 컴파일 단계에서 미리 추론이 가능해짐.
+getProperty(gli,'age')
